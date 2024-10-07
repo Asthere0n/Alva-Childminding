03fd50df7ccef8a1c838a7cadc044ee656b72635
Asthere0n <cristiansanchezsonido@gmail.com>
2024-10-08 00:19:14 +0100

    Created the Enquire page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom:['CherL', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
